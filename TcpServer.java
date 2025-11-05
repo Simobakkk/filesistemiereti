7f9eb169fdee0222b12f86118f6ae9c1cf0ada45
Simobakkk <simone.bak2007@gmail.com>
2025-11-05 22:58:20 +0100

    Esercizio A

--- TcpServer.java
+++ TcpServer.java
@@ -1,8 +1,4 @@
-
-/**
- * from network/..
- * javac network/TcpServer.java; java network.TcpServer 
- */
+package network;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -10,61 +6,43 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TcpServer {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
+		
+		int severPort=2030;
+		String clientMsg = "";
+		
+		try {			 
+			// Creazione del socket sul server e ascolto sulla porta
+			ServerSocket serverSocket = new ServerSocket(severPort);
+			System.out.println("Server: in ascolto sulla porta " + severPort);
 
-        int severPort = 8765;
-        String clientMsg = "";
-        boolean verify = false;
+			// Attesa della connessione con il client
+			Socket clientSocket = serverSocket.accept();
+			
+			// Create input and output streams to read/write data
+			DataInputStream inStream = new DataInputStream(clientSocket.getInputStream());
+			DataOutputStream outStream = new DataOutputStream(clientSocket.getOutputStream());	
 
-        try {
-            // Creazione del socket sul server e ascolto sulla porta
-            ServerSocket serverSocket = new ServerSocket(severPort);
-            System.out.println("Server: in ascolto sulla porta " + severPort);
+			// Scambio di dati tra client e server
+			while(!clientMsg.equals("quit")) {
+				//Lettura dato da stream di rete
+				clientMsg = inStream.readUTF();
+				System.out.println("Server: ricevuto messaggio " + clientMsg );
+				
+				//Invio dati su stream di rete
+				outStream.writeUTF("Echo from server : "         + clientMsg);
+				outStream.flush();
+				System.out.println("Server: invio messaggio "    + clientMsg );
+			}
 
-            // Attesa della connessione con il client
-            Socket clientSocket = serverSocket.accept();
+			// Close resources
+			serverSocket.close();
+			clientSocket.close();
+			inStream.close();
+			outStream.close();
 
-            // Create input and output streams to read/write data
-            DataInputStream inStream = new DataInputStream(clientSocket.getInputStream());
-            DataOutputStream outStream = new DataOutputStream(clientSocket.getOutputStream());
-
-            // Scambio di dati tra client e server
-            while ((!clientMsg.equals("quit")) && (verify == false)) {
-                clientMsg = inStream.readUTF();
-                System.out.println("Server: ricevuto messaggio " + clientMsg);
-                String vocali = "aeiou";
-                boolean vocalTrue = false;
-                int contV = 0, contC = 0;
-                for (int i = 0; i < clientMsg.length(); i++) {
-                    // conta consonanti
-                    for (int j = 0; j < vocali.length(); j++) {
-                        if (clientMsg.charAt(i) == vocali.charAt(j)) {
-                            contV++;
-                            vocalTrue = true;
-                            break;
-                        }
-                    }
-                    if (!vocalTrue && clientMsg.charAt(i) != ' ') {
-                        contC++;
-                    }
-                    vocalTrue = false;
-                }
-                if (contC * 2 == contV) {
-                    verify = true;
-                } else {
-                    outStream.writeUTF("Numero vocali: " + contV + " - Numero consonanti: " + contC);
-                    outStream.flush();
-                }
-            }
-
-            // Close resources
-            serverSocket.close();
-            clientSocket.close();
-            inStream.close();
-            outStream.close();
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
